2b188746821dc196fb86d334a6c39471bcf492fd
chrris99 <conforti.christian@gmail.com>
2022-11-27 00:57:48 +0100

    implement validation microservice

---
+++ src/validation/StoryGraph.Validation.Web/Controllers/ValidationController.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using StoryGraph.Validation.Domain;
+
+namespace StoryGraph.Validation.Web.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public sealed class ValidationController : ControllerBase
+{
+    private readonly IValidator<Story> _validator;
+    
+    public ValidationController(IValidator<Story> validator)
+    {
+        _validator = validator;
+    }
+    
+    [HttpPost]
+    public async Task<IActionResult> Validate([FromBody] Story story)
+    {
+        var result = await _validator.ValidateAsync(story);
+
+        if (result.IsValid) return Ok();
+        
+        var errorAggregate =
+            result.Errors.Aggregate(string.Empty, (current, error) => $"{current}\n{error.ErrorMessage}");
+
+        return BadRequest(errorAggregate);
+    }
+}
---
+++ src/validation/StoryGraph.Validation.Web/Extensions/FluentValidationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using StoryGraph.Validation.Domain.Abstractions;
+
+namespace StoryGraph.Validation.Web.Extensions;
+
+/// <summary>
+/// Contains extension methods for fluent validation.
+/// </summary>
+public static class FluentValidationExtensions
+{
+    /// <summary>
+    /// Specifies a custom domain error to use if the validation fails.
+    /// </summary>
+    /// <param name="rule">The current validation rule.</param>
+    /// <param name="error">The domain error to use.</param>
+    /// <typeparam name="T">The type being validated..</typeparam>
+    /// <typeparam name="TProperty">The property being validated.</typeparam>
+    /// <returns></returns>
+    public static IRuleBuilderOptions<T, TProperty> WithError<T, TProperty>(this IRuleBuilderOptions<T, TProperty> rule,
+        Error error)
+    {
+        ArgumentNullException.ThrowIfNull(error);
+
+        return rule.WithErrorCode(error.Code).WithMessage(error.Message);
+    }
+}
