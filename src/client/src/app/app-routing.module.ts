7dcbd62b7c58dbde829bd74ae89572563d4e175c
chrris99 <conforti.christian@gmail.com>
2022-11-26 11:49:03 +0100

    implement routing

--- src/client/src/app/app-routing.module.ts
+++ src/client/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NotFoundComponent } from './shared/errors/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./modules/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'graph', loadChildren: () => import('./modules/graph/graph.module').then(m => m.GraphModule) },
+  { path: '', redirectTo: '/graph', pathMatch: 'full' },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
