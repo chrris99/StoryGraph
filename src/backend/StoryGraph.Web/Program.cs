975ec7bc8e57820d539f29303e9bd5561fb3ceef
chrris99 <conforti.christian@gmail.com>
2022-11-27 16:27:03 +0100

    add project dependencies

--- src/backend/StoryGraph.Web/Program.cs
+++ src/backend/StoryGraph.Web/Program.cs
@@ -1,4 +1,15 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.OpenApi.Models;
 using StoryGraph.Api;
+using StoryGraph.Application;
+using StoryGraph.Application.Repositories;
+using StoryGraph.Infrastructure;
+using StoryGraph.Infrastructure.Authentication;
+using StoryGraph.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +22,91 @@ builder.Services
     .AddEndpointsApiExplorer()
     .AddSwaggerGen();
 
+// Configure Swagger
+builder.Services
+    .AddSwaggerGen(options =>
+    {
+        options.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Story Visualizer API",
+            Version = "v1"
+        });
+        options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+        {            
+            Name = "Authorization",
+            Description = "Enter a valid token",
+            In = ParameterLocation.Header,
+            Type = SecuritySchemeType.Http,
+            BearerFormat = "JWT",
+            Scheme = JwtBearerDefaults.AuthenticationScheme
+        });
+        options.AddSecurityRequirement(new OpenApiSecurityRequirement
+        {
+            {
+                new OpenApiSecurityScheme
+                {
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = JwtBearerDefaults.AuthenticationScheme
+                    }
+                },
+                new List<string>()
+            }
+        });
+    });
+
+// Configure Database Connection
+builder.Services
+    .AddDbContext<ApplicationDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+    })
+    .AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+
+// Configure Identity Options
+builder.Services
+    .Configure<IdentityOptions>(options =>
+    {
+        options.Password.RequireDigit = false;
+        options.Password.RequireLowercase = false;
+        options.Password.RequireUppercase = false;
+        options.Password.RequireNonAlphanumeric = false;
+        options.Password.RequiredLength = 8;
+
+        options.User.RequireUniqueEmail = true;
+    });
+
+// Configure JWT Authentication
+builder.Services
+    .Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Key))
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(options =>
+    {
+        var key = Encoding.UTF8.GetBytes(builder.Configuration.GetValue<string>("Jwt:Secret"));
+
+        options.SaveToken = true;
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ValidateIssuer = false, // for dev
+            ValidateAudience = false, // for dev
+            RequireExpirationTime = false, // for dev
+            ValidateLifetime = true
+        };
+    });
+
+// Configure Services
+builder.Services.AddScoped<ITokenProvider, JwtProvider>();
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -18,6 +114,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
