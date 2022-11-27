d4de5618684a5894e5e93bb20d2571b18c7a40d2
chrris99 <conforti.christian@gmail.com>
2022-11-27 16:27:24 +0100

    create dummy story controller

--- src/backend/StoryGraph.Api/Controllers/StoryController.cs
+++ src/backend/StoryGraph.Api/Controllers/StoryController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace StoryGraph.Api.Controllers;
 
@@ -7,13 +10,19 @@ public sealed class StoryController : ControllerBase
 {
     [HttpGet]
     [Route(Routes.Story.Get)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public IActionResult Get()
     {
         return Ok();
     }
-
+    
     [HttpDelete]
     [Route(Routes.Story.Delete)]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public IActionResult Delete()
     {
         return Ok();
